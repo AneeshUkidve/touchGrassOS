1b34ac0d7e88771c0a26175afb22881667797e08
AneeshUkidve <aneeshukidve@gmail.com>
2024-02-04 18:30:07 +0530

    vbr location not hardcoded anymore

--- src/boot0.s
+++ src/boot0.s
@@ -54,14 +54,13 @@ findVBR:
         mov al, 'S'
         int 0x10
         prepVBRdataPacket:
-            add bx, 11                        ;LBA address offset
-            mov eax, [bx]
-            ;cmp ax, 1;
-            ;jne debug
+            add bx, 8;                  LBA Address offset
+            mov DWORD eax, [bx]
             mov [VBRaddressLBA], eax
-            add bx, 4                        
-            mov ax, [bx]
+            add bx, 4;                  Number of sectors offset
+            mov WORD ax, [bx]
             mov [VBRsectorsNo], ax
+
         checkIfLBA:
             clc
             mov ah, 0x41
@@ -83,19 +82,12 @@ findVBR:
             cmp ah, 0
             jne diskReadError
             jmp verifyVBR
-debug:
-    mov al, ah
-    mov ah, 0x0e
-    ;mov al, 'D'
-    add al, 48
-    int 0x10
-    jmp Count
 
 useCHS:
     mov ah, 0x0e
     mov al, 'C'
     int 0x10
-    jmp Count
+    jmp end
 
 verifyVBR:
     cmp WORD [0x7DFE], 0xAA55
@@ -106,16 +98,16 @@ verifyError:
     mov ah, 0x0e
     mov al, 'V'
     int 0x10
-    jmp Count
+    jmp end
 
 diskReadError:
     mov ah, 0x0e
     mov al, 'E'
     int 0x10
-    jmp Count
+    jmp end
 noBootFound:
     mov ah, 0x0e
-    mov cx, Count
+    mov cx, end
     mov bx, noBootMsg
     jmp printMsg
 
@@ -133,27 +125,24 @@ VBRdataPacket:
     db 0x10         ;Size of packet
     db 0            ;Always 0
     VBRsectorsNo: 
-        dw 1       ;Number of sectors to read 
+        dw 0        ;Number of sectors to read 
     dw 0x7c00       ;Where in memory 0:7xc00
     dw 0            ;In which memory page
     VBRaddressLBA:
-        dd 1        ;Lower 32 bits of LBA address
+        dd 0        ;Lower 32 bits of LBA address
         dd 0        ;Upper 16 bits of "----------", Damn
 
-Count:
-    mov ah, 0x0e
-    mov al, ($-$$)-100
-    int 0x10
-
-times 0x1bc-($-$$) db 0;    Padding till disk entries
+end times 0x1bc-($-$$) db 0;    Padding till disk entries
 db 0, 0;                    reserved
-bootSector:
+;PARTITION TABLE IS """NOT""" ALIGNED ON 4 BYTE BOUNDARY
+;IT IS ENCODED IN LITTLE ENDIAN
+bootSector:                
     db 0x80;                Active (bootable)
     db 0, 2, 0;             H C S of first sector in partition
     db 0x7f;                Custom filesystem
     db 0, 11, 0;            H C S of last sector in partition
-    db 0, 0, 0, 1;          LBA of first sector
-    db 0, 0, 0, 1;         number of sectors in partition
+    dd 1;                   LBA of first sector
+    dd 10;                  number of sectors in partition
 kernelSector times 16 db 0
 deviceStorage times 16 db 0
 unused times 16 db 0
--- src/boot1.s
+++ src/boot1.s
@@ -15,7 +15,8 @@ jumpToCx:
     jmp cx
 successMessage:
     db 0xa, 0xd, "VBR Booted Successfully", 0xa, 0xd, 0;
+times 510-($-$$) db 0
+dw 0xaa55                ;Little endian for 0x55, 0xaa
 end:
     db 0
-times 510-($-$$) db 0
-db 0x55, 0xAA
+times 5120-($-$$) db 0
