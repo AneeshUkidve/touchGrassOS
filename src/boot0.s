07b90b91e2e92e99430552f1f04c8239beb63681
AneeshUkidve <aneeshukidve@gmail.com>
2024-05-29 15:46:36 +0530

    GDT  loaded, IDT space reserved

--- src/boot0.s
+++ src/boot0.s
@@ -1,8 +1,9 @@
 [bits 16]
-[org 0x0600]
+[org 0x0500]
 
 _start:
     cli;
+
     xor ax, ax;
     mov ds, ax;
     mov es, ax;
@@ -12,7 +13,7 @@ _start:
     .Relocate:
         mov cx, 256;
         mov si, 0x7c00;
-        mov di, 0x600;
+        mov di, 0x500;
         rep movsw;
     
     jmp 0:MBRbootStart
@@ -37,7 +38,7 @@ jumpToCx:
     jmp cx
 
 findVBR:
-    mov bx, bootSector
+    mov bx, ActivePartition
     mov cl, 1
     mov al, [bx]
     search:
@@ -50,16 +51,11 @@ findVBR:
         mov al, [bx]
         jmp search
     VBRfound:
-        mov ah, 0x0e
-        mov al, 'S'
-        int 0x10
         prepVBRdataPacket:
             add bx, 8;                  LBA Address offset
             mov DWORD eax, [bx]
             mov [VBRaddressLBA], eax
-            add bx, 4;                  Number of sectors offset
-            mov WORD ax, [bx]
-            mov [VBRsectorsNo], ax
+            mov WORD [VBRsectorsNo], 10 ;number of sectors to load (VBR size)
 
         checkIfLBA:
             clc
@@ -90,8 +86,9 @@ useCHS:
     jmp end
 
 verifyVBR:
-    cmp WORD [0x7DFE], 0xAA55
+    cmp WORD [0x8FFE], 0xAA55
     jne verifyError
+    mov dl, [bootDrive]
     jmp 0:0x7c00
 
 verifyError:
@@ -136,13 +133,13 @@ end times 0x1bc-($-$$) db 0;    Padding till disk entries
 db 0, 0;                    reserved
 ;PARTITION TABLE IS """NOT""" ALIGNED ON 4 BYTE BOUNDARY
 ;IT IS ENCODED IN LITTLE ENDIAN
-bootSector:                
+ActivePartition:                
     db 0x80;                Active (bootable)
     db 0, 2, 0;             H C S of first sector in partition
     db 0x7f;                Custom filesystem
     db 0, 11, 0;            H C S of last sector in partition
     dd 1;                   LBA of first sector
-    dd 10;                  number of sectors in partition
+    dd 1000;                number of sectors in partition
 kernelSector times 16 db 0
 deviceStorage times 16 db 0
 unused times 16 db 0
--- src/boot1.s
+++ src/boot1.s
@@ -1,8 +1,9 @@
 [bits 16]
 [org 0x7c00]
+mov [bootDrive], dl
 mov ah, 0x0e
 mov bx, successMessage
-mov cx, end
+mov cx, LoadGDT
 printMsg:
     mov al, [bx]
     cmp al, 0
@@ -10,13 +11,115 @@ printMsg:
     int 0x10
     inc bx
     jmp printMsg
-
 jumpToCx:
     jmp cx
+
+LoadGDT:
+    cli
+
+    writeTable:
+        mov ax, 0
+        mov es, ax
+        mov di, 0x800
+
+        nullDesc:
+            mov WORD es:[di], 0
+            mov WORD es:[di+2], 0
+            mov WORD es:[di+4], 0
+            mov WORD es:[di+6], 0
+        add di, 8
+        kernelCode:
+            mov WORD es:[di], 0xffff        ;segment limit
+            mov WORD es:[di+2], 0           ;base 15:0
+            mov BYTE es:[di+4], 0           ;base 23:15 
+            mov BYTE es:[di+5], 10011010b   ;present(1)-privilege(00)-alwaysOne(1)-code(1)-non_conforming(0)-readable(1)-beingAccessedRN(0)
+            mov BYTE es:[di+6], 11001111b   ;Granularity(1)-size32bit(1)-alwaysZero(00)-limitbits(1111)
+            mov BYTE es:[di+7], 0           ;base 31:24
+        add di, 8
+        kernelData:
+            mov WORD es:[di], 0xffff        ;segment limit
+            mov WORD es:[di+2], 0           ;base 15:0
+            mov BYTE es:[di+4], 0           ;base 23:15 
+            mov BYTE es:[di+5], 10010010b   ;present(1)-privilege(00)-alwaysOne(1)-data(0)-growUpwards(0)-writeable(1)-beingAccessedRN(0)
+            mov BYTE es:[di+6], 11001111b   ;Granularity(1)-size32bit(1)-alwaysZero(00)-limitbits(1111)
+            mov BYTE es:[di+7], 0           ;base 31:24
+        add di, 8
+        userCode:
+            mov WORD es:[di], 0xffff        ;segment limit
+            mov WORD es:[di+2], 0           ;base 15:0
+            mov BYTE es:[di+4], 0           ;base 23:15 
+            mov BYTE es:[di+5], 11111010b   ;present(1)-privilege(00)-alwaysOne(1)-code(1)-non_conforming(0)-readable(1)-beingAccessedRN(0)
+            mov BYTE es:[di+6], 11001111b   ;Granularity(1)-size32bit(1)-alwaysZero(00)-limitbits(1111)
+            mov BYTE es:[di+7], 0           ;base 31:24
+        add di, 8
+        userData:
+            mov WORD es:[di], 0xffff        ;segment limit
+            mov WORD es:[di+2], 0           ;base 15:0
+            mov BYTE es:[di+4], 0           ;base 23:15 
+            mov BYTE es:[di+5], 11110010b   ;present(1)-privilege(00)-alwaysOne(1)-data(0)-growUpwards(0)-writeable(1)-beingAccessedRN(0)
+            mov BYTE es:[di+6], 11001111b   ;Granularity(1)-size32bit(1)-alwaysZero(00)-limitbits(1111)
+            mov BYTE es:[di+7], 0           ;base 31:24
+        add di, 8
+        taskStateSegment:
+            mov WORD es:[di], 104
+            mov WORD es:[di+2], 0x1000
+            mov BYTE es:[di+4], 0
+            mov BYTE es:[di+5], 0x89
+            mov BYTE es:[di+6], 0x40
+            mov BYTE es:[di+7], 0
+
+    lgdt [gdtptr]
+    jmp setUpTSS
+
+setUpTSS:
+    ;clear destination
+    mov cx, 52
+    mov di, 0x1000
+    xor ax, ax
+    rep stosw
+
+    ;set Value of ESP0          (empty stack pointer of kernel level i.e. ring 0)
+    mov DWORD es:[di+4], 0x7ffff
+    ;set value of SS0           (segment selector offset for kernelData)
+    mov WORD  es:[di+8], 0x10
+    ;set value of IOBP          (Archaic thing no longer necessary?)
+    mov WORD es:[di+108], 104
+
+    ;THE LOADING OF TSS TAKES PLACE IN PROTECTED MODE, NOT REAL MODE, WHICH IS WHY IT WASNT DONE HERE
+
+    jmp loadIDT
+
+loadIDT:
+    ;We shall simply reserve 2048 bytes for our IDT, the table can then be filled in once we're in the kernel
+    ;Clear out 512 DWORDS from 0x700 - 0xF00
+    mov cx, 512
+    mov di, 0x700
+    xor eax, eax
+    rep stosd
+
+    lidt [idtptr]   ;Load
+    mov ah, 0x0e
+    mov al, "L"
+    int 0x10
+    jmp end
+    ; mov cx, end
+    ; mov ah, 0x0e
+    ; mov bx, greatSuccess
+    ; jmp printMsg
 successMessage:
-    db 0xa, 0xd, "VBR Booted Successfully", 0xa, 0xd, 0;
-times 510-($-$$) db 0
-dw 0xaa55                ;Little endian for 0x55, 0xaa
+    db 0xa, 0xd, "VBR Booted Succesfully", 0x0a, 0x0d, 0;
+greatSuccess:
+    db "Great Success", 0xa, 0xd, 0;
+idtptr:
+    dw 2047     ;Size of table
+    dd 0x700    ;Where is it
+gdtptr:
+    dw 47       ;size(GDT) - 1
+    dd 0xF00    ;location of GDT
+
+bootDrive:
+    db 0
 end:
     db 0
-times 5120-($-$$) db 0
+times 5118-($-$$) db 0
+dw 0xaa55
